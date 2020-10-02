d8f3199681174c24eb296ce6e9407244290f8747
Arkadiusz Masiakiewicz <arkadiusz.masiakiewicz@avra.pl>
2020-10-02 17:16:39 +0200

    adding swagger doc

--- src/main/java/com/casinoroyale/transfer/TransferApplication.java
+++ src/main/java/com/casinoroyale/transfer/TransferApplication.java
@@ -4,6 +4,11 @@ import java.time.ZoneOffset;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class TransferApplication {
@@ -11,7 +16,16 @@ public class TransferApplication {
     public static final ZoneOffset DEFAULT_ZONE_OFFSET = ZoneOffset.UTC;
 
     public static void main(String[] args) {
-		SpringApplication.run(TransferApplication.class, args);
-	}
-	
+        SpringApplication.run(TransferApplication.class, args);
+    }
+
+    @Bean
+    Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 }
